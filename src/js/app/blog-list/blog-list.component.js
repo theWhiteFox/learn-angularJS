669cea6605f5ec5db06648f478c9c4ed19f314a6
Ó Conchubhair <ste@thewhitefox.ninja>
2018-05-14 15:29:59 +0100

    :pear: templates basic :metal:

---
+++ src/js/app/blog-list/blog-list.component.js
@@ -0,0 +1,28 @@
+(function() {
+  "use strict";
+
+  angular.module("blogList")
+  .component("blogList", {
+    // template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>", 
+    templateUrl: "templates/blog-list.html",
+    controller: function($scope) {
+      var blogItems = [
+        {title: "Some Title", id: 1, description: "This is a book", publishDate: "2018-05-14"},
+        {title: "Title", id: 2, description: "This is a book"},
+        {title: "Tea", id: 3, description: "This is a book"},
+        {title: "Coffee", id: 4, description: "This is a book"}        
+      ];
+
+      $scope.items = blogItems;
+
+      console.log("Hello!");
+      $scope.title = "Hi there";
+      $scope.clicks = 0;
+      $scope.someClickTest = () => {
+        console.log("clicked");
+        $scope.clicks += 1;
+        $scope.title = "Clicked " + $scope.clicks + " times";
+      };
+    }
+  });
+})();
