b40a84f9f4bc78012a05e41787d2df3711a59c11
Ó Conchubhair <ste@thewhitefox.ninja>
2018-05-19 18:45:08 +0100

    :beers: http :cake:

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,35 +3,45 @@
 
   angular.module("blogDetail").component("blogDetail", {
     templateUrl: "templates/blog-detail.html",
-    controller: function($location, $routeParams, $scope) {
-      var blogItems = [
-        {
-          title: "Some Title",
-          id: 1,
-          description: "This is a book",
-          publishDate: "2018-05-14"
-        },
-        { title: "Title", id: 2, description: "This is a book" },
-        { title: "Tea", id: 3, description: "This is a book" },
-        { title: "Coffee", id: 4, description: "This is a book" }
-      ];
+    controller: function($http, $location, $routeParams, $scope) {
+      $http.get("/json/post.json").then(successCallBack, errorCallback);
 
-      // $scope.items = blogItems;
-      // console.log($routeParams);
-      $scope.title = "Blog " + $routeParams.id;
-      $scope.notFound = true;
-      angular.forEach(blogItems, post => {
-        if (post.id == $routeParams.id) {
-          $scope.notFound = false;
-          $scope.post = post;
-        }        
-      });
+      function successCallBack(response, status, config, statusText) {
+        $scope.notFound = true;
+        var blogItems = response.data;
+        $scope.posts = blogItems;
+        angular.forEach(blogItems, post => {
+          if (post.id == $routeParams.id) {
+            $scope.notFound = false;
+            $scope.post = post;
+          }
+        });
+      }
+
+      function errorCallback() {
+        $scope.notFound = true;
+        console.log(response);
+      }
 
       if ($scope.notFound) {
         console.log("Not Found");
         // change location
-        $location.path("/404");
+        $location.path("/");
       }
+
+      // var blogItems = [
+      //   {
+      //     title: "Some Title",
+      //     id: 1,
+      //     description: "This is a book",
+      //     publishDate: "2018-05-14"
+      //   },
+      //   { title: "Title", id: 2, description: "This is a book" },
+      //   { title: "Tea", id: 3, description: "This is a book" },
+      //   { title: "Coffee", id: 4, description: "This is a book" }
+      // ];
+
+      // $scope.title = "Blog " + $routeParams.id;
     }
   });
 })();
